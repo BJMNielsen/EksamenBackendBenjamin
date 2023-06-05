e6bf6e7dbe45c859a76643380082cf75af9178f1
benj302g <benj302g@stud.kea.dk>
2023-06-05 19:43:22 +0200

    Init data ændret

--- src/main/java/com/example/eksamenbackendbenjamin/config/StartupDataConfig.java
+++ src/main/java/com/example/eksamenbackendbenjamin/config/StartupDataConfig.java
@@ -14,9 +14,10 @@ import org.springframework.context.annotation.Configuration;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Configuration
-public class StartupDataConfig implements ApplicationRunner{
+public class StartupDataConfig implements ApplicationRunner {
 
     @Autowired
     SailBoatRepository sailBoatRepository;
@@ -26,7 +27,6 @@ public class StartupDataConfig implements ApplicationRunner{
 
     @Autowired
     RaceParticipationRepository raceParticipationRepository;
-
 
 
     @Override
@@ -40,20 +40,20 @@ public class StartupDataConfig implements ApplicationRunner{
         SailBoat sailBoatForEveryRace = new SailBoat("Uma Jolie", "25fod");
         createSailBoatInEveryRace(sailBoatForEveryRace);
 
-        for (SailBoat sailBoat: listOfBoats){
+        for (SailBoat sailBoat : listOfBoats) {
             System.out.println(sailBoat.getId() + ": " + sailBoat.getName());
-            createRaceParticipation(sailBoat);
+            //createRaceParticipation(sailBoat);
         }
     }
 
     //Skriv en metode der kan oprette en sejlbåd som deltager i alle kapsejladser.
-    public void createSailBoatInEveryRace(SailBoat sailBoat){
+    public void createSailBoatInEveryRace(SailBoat sailBoat) {
 
         SailBoat newSailBoat = sailBoatRepository.save(sailBoat);
 
         List<SailRace> allRaces = sailRaceRepository.findAll();
 
-        for (SailRace race: allRaces){
+        for (SailRace race : allRaces) {
             RaceParticipation raceParticipation = new RaceParticipation();
             raceParticipation.setSailBoat(newSailBoat);
             raceParticipation.setSailRace(race);
@@ -61,11 +61,11 @@ public class StartupDataConfig implements ApplicationRunner{
         }
     }
 
-    public void createRaceParticipation(SailBoat sailBoat){
+    public void createRaceParticipation(SailBoat sailBoat) {
         List<SailRace> allRaces = sailRaceRepository.findAll();
         boolean unEqualNumber = sailBoat.getId() % 2 != 0;
 
-        if(unEqualNumber){
+        if (unEqualNumber) {
             SailRace ultimateRace = allRaces.get(0);
 
             RaceParticipation raceParticipationUltimateRace = new RaceParticipation();
@@ -92,7 +92,7 @@ public class StartupDataConfig implements ApplicationRunner{
         }
     }
 
-    public List<SailBoat> createSailBoats(){
+    public List<SailBoat> createSailBoats() {
 
         List<SailBoat> listOfBoats = new ArrayList<>();
 
@@ -112,20 +112,69 @@ public class StartupDataConfig implements ApplicationRunner{
         return listOfBoats;
     }
 
-    public void createRaces(){
-        SailRace ultimateRace = new SailRace();
-        ultimateRace.setName("Ultimate Race");
-        ultimateRace.setSailRaceDate(LocalDate.now());
-        sailRaceRepository.save(ultimateRace);
+    public LocalDate generateRandomDate () {
+        LocalDate currentDate = LocalDate.now(); // Get the current date
 
-        SailRace børgesSejlKonkurrence = new SailRace();
-        børgesSejlKonkurrence.setName("Børges Sejlkonkurrence");
-        børgesSejlKonkurrence.setSailRaceDate(LocalDate.now());
-        sailRaceRepository.save(børgesSejlKonkurrence);
+        // Generate a random number of days to add to the current date
+        Random random = new Random();
+        int randomDays = random.nextInt(365); // Generate a random number between 0 and 364
 
-        SailRace eksamensSejllads = new SailRace();
-        eksamensSejllads.setName("Eksamens Store Sejlladskonkurrence");
-        eksamensSejllads.setSailRaceDate(LocalDate.now());
-        sailRaceRepository.save(eksamensSejllads);
+        LocalDate futureDate = currentDate.plusDays(randomDays); // Add the random number of days to the current date
+        return futureDate;
+    }
+
+    public void createRaces() {
+
+        LocalDate dateForUltimateRace = generateRandomDate();
+
+        SailRace ultimateRace25fod = new SailRace();
+        ultimateRace25fod.setName("Ultimate Race 25fod");
+        ultimateRace25fod.setSailRaceDate(dateForUltimateRace);
+        sailRaceRepository.save(ultimateRace25fod);
+
+        SailRace ultimateRace2540 = new SailRace();
+        ultimateRace2540.setName("Ultimate Race 25-40fod");
+        ultimateRace2540.setSailRaceDate(dateForUltimateRace);
+        sailRaceRepository.save(ultimateRace2540);
+
+
+        SailRace ultimateRace40 = new SailRace();
+        ultimateRace40.setName("Ultimate Race 40fod");
+        ultimateRace40.setSailRaceDate(dateForUltimateRace);
+        sailRaceRepository.save(ultimateRace40);
+
+        LocalDate dateBørge = generateRandomDate();
+
+        SailRace børgesSejlKonkurrence25 = new SailRace();
+        børgesSejlKonkurrence25.setName("Børges Sejlkonkurrence 25fod");
+        børgesSejlKonkurrence25.setSailRaceDate(dateBørge);
+        sailRaceRepository.save(børgesSejlKonkurrence25);
+
+        SailRace børgesSejlKonkurrence2540 = new SailRace();
+        børgesSejlKonkurrence2540.setName("Børges Sejlkonkurrence 25-40fod");
+        børgesSejlKonkurrence2540.setSailRaceDate(dateBørge);
+        sailRaceRepository.save(børgesSejlKonkurrence2540);
+
+        SailRace børgesSejlKonkurrence40 = new SailRace();
+        børgesSejlKonkurrence40.setName("Børges Sejlkonkurrence 40fod");
+        børgesSejlKonkurrence40.setSailRaceDate(dateBørge);
+        sailRaceRepository.save(børgesSejlKonkurrence40);
+
+
+        LocalDate dateEksamensSejlads = generateRandomDate();
+        SailRace eksamensSejllads25 = new SailRace();
+        eksamensSejllads25.setName("Eksamens Store Sejlladskonkurrence 25fod");
+        eksamensSejllads25.setSailRaceDate(dateEksamensSejlads);
+        sailRaceRepository.save(eksamensSejllads25);
+
+        SailRace eksamensSejllads2540 = new SailRace();
+        eksamensSejllads2540.setName("Eksamens Store Sejlladskonkurrence 25-40fod");
+        eksamensSejllads2540.setSailRaceDate(dateEksamensSejlads);
+        sailRaceRepository.save(eksamensSejllads2540);
+
+        SailRace eksamensSejllads40 = new SailRace();
+        eksamensSejllads40.setName("Eksamens Store Sejlladskonkurrence 40fod");
+        eksamensSejllads40.setSailRaceDate(dateEksamensSejlads);
+        sailRaceRepository.save(eksamensSejllads40);
     }
 }
--- src/main/java/com/example/eksamenbackendbenjamin/enums/BoatType.java
+++ src/main/java/com/example/eksamenbackendbenjamin/enums/BoatType.java
@@ -1,7 +1,19 @@
 package com.example.eksamenbackendbenjamin.enums;
 
 public enum BoatType {
-    LARGE,
-    MEDIUM,
-    SMALL
+    FEMOGTYVEFOD("25fod"),
+    FEMOGTYVETILFYRREFOD("25-40fod"),
+    FYRREFOD("40fod");
+
+    private final String description;
+
+
+    BoatType(String description) {
+        this.description = description;
+
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
